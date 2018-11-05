3d3228ad2abee541c1133a4757c598bc8f2afe7b
Lucas Soares <44124764+lucas2707@users.noreply.github.com>
2018-11-05 21:44:56 -0200

    Add files via upload

---
+++ aula17/017_VerdadeiroFalso/src/controller/Teste.java
@@ -0,0 +1,20 @@
+package controller;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		boolean b = false;
+		boolean a = true;
+		boolean c = a && !b;
+		if (c) 
+		{
+			System.out.println(c);
+		}
+		/*
+		 * } else {
+		 * 
+		 * System.out.println("Diferentes"); }
+		 */
+	}
+}
