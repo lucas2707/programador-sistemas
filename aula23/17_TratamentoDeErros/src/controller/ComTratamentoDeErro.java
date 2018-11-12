1aa0ddb06403820b7529fdd52dd4441bb7e8fd1e
Lucas Soares <44124764+lucas2707@users.noreply.github.com>
2018-11-12 21:31:06 -0200

    Add files via upload

---
+++ aula23/17_TratamentoDeErros/src/controller/ComTratamentoDeErro.java
@@ -0,0 +1,27 @@
+package controller;
+
+import util.Teclado;
+
+public class ComTratamentoDeErro {
+
+	public static void main(String[] args) {
+		try {
+		
+		double a = Teclado.lerDouble("Digite o Numerador: ");
+		double b = Teclado.lerDouble("Digite o Denominador: ");
+
+		double c = a / b;
+		System.out.println("Resultado da divisão de a por b: " + c);
+		
+		
+	} catch (NumberFormatException nfe) {
+		System.out.println("Caracter inválido! ");
+	   
+	} catch (ArithmeticException ae) {
+		System.out.println("Divisão por Zero");
+		
+		
+		
+	}
+	}
+}
